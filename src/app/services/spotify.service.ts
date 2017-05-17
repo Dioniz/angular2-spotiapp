d6d20ec9f5d630b4eb23e0dba73106a4a747f5e2
Fran <frandioniz@gmail.com>
2017-05-17 12:22:28 +0100

    SpotiApp

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class SpotifyService {
+
+  private artistas:any[] = [];
+  private urlBusqueda:string = "https://api.spotify.com/v1/search";
+  private urlArtista:string = "https://api.spotify.com/v1/artists";
+
+  constructor( private http:Http ) { }
+
+
+  getArtistas( termino:string ) {
+    let query = `?q=${ termino }&type=artist`;
+    let url = this.urlBusqueda + query;
+
+    return  this.http.get( url )
+              .map( res =>{
+                this.artistas = res.json().artists.items;
+                console.log(this.artistas);
+                return this.artistas;
+              })
+
+  }
+
+  getArtista( id:string ) {
+    let query = `/${ id }`;
+    let url = this.urlArtista + query;
+
+    return  this.http.get( url )
+              .map( res =>{
+                console.log(res.json());
+                return res.json();
+              })
+
+  }
+
+  getTop( id:string ) {
+    let query = `/${ id }/top-tracks?country=US`;
+    let url = this.urlArtista + query;
+
+    return  this.http.get( url )
+              .map( res =>{
+                console.log(res.json().tracks);
+                return res.json().tracks;
+              })
+
+  }
+
+}
